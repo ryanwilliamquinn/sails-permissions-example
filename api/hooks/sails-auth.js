20bfae2794dd26436d92da7becab75601eced909
ryan quinn <ryanwilliamquinn@gmail.com>
2015-07-27 11:30:33 -0400

    fixes for latest sails permissions

--- api/hooks/sails-auth.js
+++
@@ -1,4 +0,0 @@
-// api/hooks/sails-auth.js
-
-
-module.exports = require('sails-permissions/api/hooks/sails-auth');
--- api/hooks/sails-permissions.js
+++
@@ -1,4 +0,0 @@
-// api/hooks/sails-permissions.js
-
-
-module.exports = require('sails-permissions/api/hooks/sails-permissions');
